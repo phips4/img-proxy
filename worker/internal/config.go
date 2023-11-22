b6e13ee3e756093ba2d652df09c87f503111bdc7
phips4 <phips4.yt@gmail.com>
2023-11-22 02:13:59 +0100

    implement AppConfig

---
+++ worker/internal/config.go
@@ -0,0 +1,66 @@
+package internal
+
+import (
+	"errors"
+	"os"
+	"strings"
+)
+
+type AppConfig struct {
+	clusterSecret []byte
+	host          string
+	port          string
+	knownHosts    []string
+	name          string
+}
+
+func ConfigFromEnv() (*AppConfig, error) {
+	conf := &AppConfig{}
+
+	conf.clusterSecret = []byte(os.Getenv("CLUSTER_SECRET"))
+	if conf.clusterSecret == nil || len(conf.clusterSecret) <= 0 {
+		return nil, errors.New("env CLUSTER_SECRET is not set")
+	}
+
+	conf.host = os.Getenv("HOST")
+	if conf.host == "" {
+		return nil, errors.New("env HOST is not set")
+	}
+
+	conf.port = os.Getenv("PORT")
+	if conf.port == "" {
+		return nil, errors.New("env PORT is not set")
+	}
+
+	conf.knownHosts = strings.Split(os.Getenv("KNOWN_HOSTS"), ",")
+	if len(conf.knownHosts) <= 0 {
+		return nil, errors.New("env KNOWN_HOSTS is not set")
+	}
+
+	conf.name = os.Getenv("NAME")
+	if conf.name == "" {
+		conf.name = conf.host
+	}
+
+	return conf, nil
+}
+
+func (c *AppConfig) ClusterSecret() []byte {
+	return c.clusterSecret
+}
+
+func (c *AppConfig) Host() string {
+	return c.host
+}
+
+func (c *AppConfig) Port() string {
+	return c.port
+}
+
+func (c *AppConfig) KnownHosts() []string {
+	return c.knownHosts
+}
+
+func (c *AppConfig) Name() string {
+	return c.name
+}
