e0fb8779a9de32b52bd1827c70b9f0a5d68d0f0d
phips4 <phips4.yt@gmail.com>
2024-06-20 00:06:06 +0200

    refactor logic from worker main to middleware package

---
+++ worker/internal/middleware/exclusive.go
@@ -0,0 +1,23 @@
+package middleware
+
+import "net/http"
+
+func OnlyGet(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		next(w, r)
+	}
+}
+
+func OnlyPost(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		next(w, r)
+	}
+}
